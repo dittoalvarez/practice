cfa7c88368f245978271af264a3952ae061d9ad9
dittochippit <119551313+dittochippit@users.noreply.github.com>
2022-12-07 13:48:32 +0800

    secondary commit

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ export default function App() {
   }
 
   function addGoalHandler() {
-    setArrayItem((e) => [...arrayItem, inputGoal]);
+    setArrayItem((newArrayItem) => [...newArrayItem, inputGoal]);
   }
   return (
     <View style={styles.mainContainer}>
@@ -43,8 +43,10 @@ const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: "flex-start",
+    justifyContent: "space-around",
+    margin: 20,
+    padding: 10,
   },
   input: {
     width: "70%",
@@ -54,18 +56,18 @@ const styles = StyleSheet.create({
   button: {
     width: "20%",
     flex: 1,
-    borderWidth: 1,
+    //borderWidth: 1,
     borderRadius: 20,
     alignItems: "center",
     justifyContent: "center",
-    padding: 5,
-    margin: 5,
+    //paddingVertical: 0,
+    //marginVertical: 0,
   },
   container: {
     flex: 1,
     flexDirection: "row",
     justifyContent: "space-between",
-    width: "90%",
+    width: "100%",
     borderBottomWidth: 1,
     borderColor: "#CCCCCC",
     paddingBottom: 20,
