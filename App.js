8a0b30884114935cab3126ba6012623b856ecca7
ditto <dittoalvarez@gmail.com>
2022-12-07 20:38:12 +0800

    optimized code

--- App.js
+++ App.js
@@ -5,6 +5,8 @@ import {
   TextInput,
   TouchableOpacity,
   View,
+  ScrollView,
+  FlatList,
 } from "react-native";
 
 export default function App() {
@@ -16,24 +18,38 @@ export default function App() {
   }
 
   function addGoalHandler() {
-    setArrayItem((newArrayItem) => [...newArrayItem, inputGoal]);
+    setArrayItem((newArrayItem) => [
+      ...newArrayItem,
+      { text: inputGoal, id: Math.random().toString() },
+    ]);
   }
   return (
     <View style={styles.mainContainer}>
       <View style={styles.container}>
-        <TextInput
-          style={styles.input}
-          placeholder="Enter Text..."
-          onChangeText={inputHandler}
-        />
-        <TouchableOpacity style={styles.button} onPress={addGoalHandler}>
-          <Text>Add Button</Text>
-        </TouchableOpacity>
+        <View style={styles.input}>
+          <TextInput placeholder="Enter Text..." onChangeText={inputHandler} />
+        </View>
+
+        <View style={styles.button}>
+          <TouchableOpacity title={"Add Button"} onPress={addGoalHandler}>
+            <Text>Add Button</Text>
+          </TouchableOpacity>
+        </View>
       </View>
       <View style={styles.bottomContainer}>
-        {arrayItem.map((item) => (
-          <Text key={item}>{item}</Text>
-        ))}
+        <FlatList
+          data={arrayItem}
+          keyExtractor={(item, index) => {
+            return item.id;
+          }}
+          renderItem={(itemData) => {
+            return (
+              <View>
+                <Text>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+        />
       </View>
     </View>
   );
@@ -49,19 +65,22 @@ const styles = StyleSheet.create({
     padding: 10,
   },
   input: {
+    flex: 2,
     width: "70%",
+    justifyContent: "center",
     marginVertical: 2,
     paddingVertical: 2,
+    borderWidth: 1,
   },
   button: {
-    width: "20%",
+    //width: "20%",
     flex: 1,
-    //borderWidth: 1,
-    borderRadius: 30,
+    borderWidth: 1,
+    borderRadius: 20,
     alignItems: "center",
     justifyContent: "center",
-    //paddingVertical: 0,
-    //marginVertical: 0,
+    paddingVertical: 0,
+    marginVertical: 0,
   },
   container: {
     flex: 1,
@@ -78,5 +97,14 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     alignItems: "flex-start",
     justifyContent: "flex-start",
+    width: "100%",
+  },
+  items: {
+    margin: 8,
+    padding: 8,
+    borderRadius: 6,
+    backgroundColor: "violet",
+    color: "white",
+    width: "100%",
   },
 });
